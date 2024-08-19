51432087ac3c9a40c2db00c2126014f935f1c34e
Ruslan Amirov <gumagereiro@rambler.ru>
2024-08-19 10:07:29 +0300

    Task 002 solution

--- app/src/androidTest/java/ru/easycode/zerotoheroandroidtdd/Task002Test.kt
+++ app/src/androidTest/java/ru/easycode/zerotoheroandroidtdd/Task002Test.kt
@@ -19,6 +19,6 @@ class Task002Test {
 
     @Test
     fun test_add_id() {
-        onView(withId(R.id.titleTextView)).check(matches(withText("I am an Android Developer!")))
+        onView(withId(R.id.titleTextView)).check(matches(withText("Hello World!")))
     }
 }
