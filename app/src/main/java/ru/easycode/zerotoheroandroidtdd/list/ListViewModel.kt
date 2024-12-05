0a9613a2cbf3255e3b90b23a72ef965626f74308
Ruslan Amirov <gumagereiro@rambler.ru>
2024-12-05 22:37:52 +0300

    Task 025 solution

---
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/list/ListViewModel.kt
@@ -0,0 +1,29 @@
+package ru.easycode.zerotoheroandroidtdd.list
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import ru.easycode.zerotoheroandroidtdd.create.CreateScreen
+import ru.easycode.zerotoheroandroidtdd.main.Navigation
+
+class ListViewModel(
+    private val liveDataWrapper: ListLiveDataWrapper.Mutable,
+    private val navigation: Navigation.Update
+): ViewModel(), ListLiveDataWrapper.Read {
+
+    fun create() {
+        navigation.update(CreateScreen)
+    }
+
+    override fun liveData(): LiveData<List<CharSequence>> {
+        return liveDataWrapper.liveData()
+    }
+
+    fun save(bundleWrapper: BundleWrapper.Save) {
+        liveDataWrapper.save(bundleWrapper)
+    }
+
+    fun restore(bundleWrapper: BundleWrapper.Restore) {
+        val list = bundleWrapper.restore()
+        liveDataWrapper.update(list)
+    }
+}
---
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/main/MainViewModel.kt
@@ -0,0 +1,19 @@
+package ru.easycode.zerotoheroandroidtdd.main
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import ru.easycode.zerotoheroandroidtdd.list.ListScreen
+
+class MainViewModel(
+    private val navigation: Navigation.Mutable
+): ViewModel(), Navigation.Read {
+
+    override fun liveData(): LiveData<Screen> {
+        return navigation.liveData()
+    }
+    fun init(firstRun: Boolean) {
+        if (firstRun) {
+            navigation.update(ListScreen)
+        }
+    }
+}
